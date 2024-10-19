6978a63889f16e90d015d56df60cf415f6cac5a3
manahillsajid <manahill.sajid@mail.utoronto.ca>
2024-10-19 19:06:10 -0400

    Completed task 2.2 of phase 1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,5 +27,15 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user.
+     * @param name the name of the user
+     */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the current user.
+     * @return the current user
+     */
+    String getCurrentUser();
 }
